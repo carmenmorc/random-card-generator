bf987fff207ac4be6f18ca1010dba33dfc05cda6
Carmen Morcuende <166156217+carmenmorc@users.noreply.github.com>
2024-06-14 19:32:55 +0000

    JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(GenerateRandomSuit());
+  document.querySelector(".number").innerHTML = GenerateRandomNumber();
+
+  function GenerateRandomNumber() {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+    return numbers[indexNumbers];
+  }
+
+  function GenerateRandomSuit() {
+    let suit = ["diamond", "heart", "spade", "club"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+
+    return suit[indexSuit];
+  }
 };
